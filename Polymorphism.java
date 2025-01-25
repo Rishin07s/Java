2da074632a24011a4898002ae4acc92a6b83e690
Rishin <rishinnethajis@gmail.com>
2025-01-25 10:35:43 +0530

    new

---
+++ Polymorphism.java
@@ -0,0 +1,34 @@
+package Java;
+class Shape {
+    public void draw() {
+        System.out.println("Drawing a shape");
+    }
+}
+
+class Circle extends Shape {
+    @Override
+    public void draw() {
+        System.out.println("Drawing a circle");
+    }
+}
+
+class Rectangle extends Shape {
+    @Override
+    public void draw() {
+        System.out.println("Drawing a rectangle");
+    }
+}
+
+public class Polymorphism {
+    public static void main(String[] args) {
+        Shape shape = new Shape();
+        shape.draw();
+
+        Shape circle = new Circle();
+        circle.draw();
+
+        Shape rectangle = new Rectangle();
+        rectangle.draw();
+    }
+}
+
